20b1193307354ef06a04f94fc46f5d717b88ec4c
melonard <matthew.leonard@aquaq.co.uk>
2021-08-03 11:19:30 +0100

    Displays the output of a qrest query

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+
+
+  constructor () {
+    super()
+    this.state = {
+        result: []
+    }
 }
 
-export default App;
+async componentDidMount() {
+    const url = "https://homer.aquaq.co.uk:8040/executeQuery";
+    const response = await 
+    fetch (url,{
+          "body": JSON.stringify({
+            "query": "select last price by sym from trade",
+            "response": "true",
+            "type": "sync"
+            }),
+          method:"post",
+          "headers": {
+            'Accept': 'application/json',
+            "Content-Type":"application/json",
+            "accept": "*/*",
+            "Authorization":"Basic dXNlcjpwYXNz"
+    }}
+    ) 
+    const data = await response.json();
+    console.log(data.result)
+    this.setState({result: data.result})
+}
+
+
+
+render() {
+    return (
+        <div>
+          <div>{JSON.stringify(this.state.result)}</div>
+        </div>
+    )
+}
+}
