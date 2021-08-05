9d6bd341c1b477e44979ad1935494621360f27f1
melonard <matthew.leonard@aquaq.co.uk>
2021-08-05 14:18:10 +0100

    Added functionality to update Most Traded Sym table each second

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -14,27 +14,32 @@ export default class App extends React.Component {
 
 async componentDidMount() {
     const url = "https://homer.aquaq.co.uk:8040/executeFunction";
-    const response = await 
-    fetch (url,{
-          "body": JSON.stringify({
-            "arguments": {
-           "db":"rdb",
-           "query":"select from (select from select sum size by sym from trade) where size = max size"},
-            "function_name": ".aqrest.execute"
-          }),
-          method:"post",
-          "headers": {
-            'Accept': 'application/json',
-            "Content-Type":"application/json",
-            "accept": "*/*",
-            "Authorization":"Basic dXNlcjpwYXNz"
-    }}
-    ) 
-    const data = await response.json();
-    console.log(data.result[0].sym)
-    this.setState({sym: data.result[0].sym})
-    this.setState({size: data.result[0].size})
-
+    try {
+      setInterval(async () => {
+        const response = await 
+        fetch (url,{
+              "body": JSON.stringify({
+                "arguments": {
+              "db":"rdb",
+              "query":"select from (select from select sum size by sym from trade) where size = max size"},
+                "function_name": ".aqrest.execute"
+              }),
+              method:"post",
+              "headers": {
+                'Accept': 'application/json',
+                "Content-Type":"application/json",
+                "accept": "*/*",
+                "Authorization":"Basic dXNlcjpwYXNz"
+        }}
+        ) 
+        const data = await response.json();
+        console.log(data.result[0].sym)
+        this.setState({sym: data.result[0].sym})
+        this.setState({size: data.result[0].size})
+      },1000);
+    } catch(e) {
+      console.log(e);
+    }
 }
 
 
