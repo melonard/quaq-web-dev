8e543224706e484fc84ca84c2bc2d7867e75c7a9
SeanW17 <sean.wilson@aquaq.co.uk>
2021-08-05 12:57:46 +0100

    Update LastValueCache.js
    Updated LVC with updating data hopefully

--- src/components/app/LastValueCache.js
+++ src/components/app/LastValueCache.js
@@ -1,15 +1,58 @@
 import React from 'react';
 import './App.css'
 
-function LastValueCache(){
-    return(
-        <div className="LVC">
-            <h3>Sym</h3>
-            <p>Current Price</p>
-            <h5>Value up or down</h5>
-        </div>
-
-    );
+export default class LastValueCache extends React.Component {
+constructor () {
+    super()
+    this.state = {
+        sym : "",
+        result:[]
+    }
+  
 }
 
-export default LastValueCache;
+async  componentDidMount1() {
+
+    const url = "https://homer.aquaq.co.uk:8040/executeQuery";
+    const response = await 
+    fetch (url,{
+          "body": JSON.stringify({
+            "query": "select diff:first price - last price by sym from trade",
+            "response": "true",
+            "type": "sync"
+            }),
+          method:"post",
+          "headers": {
+            'Accept': 'application/json',
+            "Content-Type":"application/json",
+            "accept": "*/*",
+            "Authorization":"Basic dXNlcjpwYXNz"
+    }}
+    ) 
+    const data = await response.json();
+    //console.log(data)
+    this.setState({all_data: data.result})
+    this.setState({sym: data.result.sym})
+    this.setState({diff: data.result.diff})
+}
+render() {
+    return (
+        <div className="LVC">
+            <h3>{JSON.stringify(this.state.sym)}</h3>
+            <p>Current Price</p>
+            <h5>{JSON.stringify(this.state.diff)}</h5>
+        </div>
+    )
+  }
+}
+// function LastValueCache(){
+//     return(
+//         <div className="LVC">
+//             <h3>{JSON.stringify(this.state.sym)}</h3>
+//             <p>Current Price</p>
+//             <h5>{JSON.stringify(this.state.diff)}</h5>
+//         </div>
+
+//     );
+// }
+// export default LastValueCache;
