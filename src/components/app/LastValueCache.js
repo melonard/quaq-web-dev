35c33b0b5c66c4e5e2403efbd5c85bd993b55f92
melonard <matthew.leonard@aquaq.co.uk>
2021-08-06 11:37:38 +0100

    All prices display format needs fixing

--- src/components/app/LastValueCache.js
+++ src/components/app/LastValueCache.js
@@ -5,7 +5,8 @@ export default class LastValueCache extends React.Component {
 constructor () {
     super()
     this.state = {
-        
+        sym:[],
+        diff:[],
         result:[]
     }
   
@@ -33,8 +34,10 @@ async  componentDidMount() {
     const data = await response.json();
     console.log(data)
     this.setState({all_data: data.result})
-    this.setState({sym: data.result[1].sym})
-    this.setState({diff: data.result[1].diff})
+    for (let i = 0;i<10;i++){
+        this.setState({sym:[...this.state.sym ,data.result[i].sym]})
+        this.setState({diff: [...this.state.diff,data.result[i].diff]})
+    }
 }
 
 // if (this.state.diff >= 0) {
@@ -47,9 +50,36 @@ render() {
     return (
         <div className="LVC">
             <div>{JSON.stringify(this.state.all_data)}</div>
-            <h3>{JSON.stringify(this.state.sym)}</h3>
+            <h3>{JSON.stringify(this.state.sym[0])}</h3>
             <p>Current Price</p>
-            <h5>{JSON.stringify(this.state.diff)}</h5>
+            <h5>{JSON.stringify(this.state.diff[0])}</h5>
+            <h3>{JSON.stringify(this.state.sym[1])}</h3>
+            <p>Current Price</p>
+            <h5>{JSON.stringify(this.state.diff[1])}</h5>
+            <h3>{JSON.stringify(this.state.sym[2])}</h3>
+            <p>Current Price</p>
+            <h5>{JSON.stringify(this.state.diff[2])}</h5>
+            <h3>{JSON.stringify(this.state.sym[3])}</h3>
+            <p>Current Price</p>
+            <h5>{JSON.stringify(this.state.diff[3])}</h5>
+            <h3>{JSON.stringify(this.state.sym[4])}</h3>
+            <p>Current Price</p>
+            <h5>{JSON.stringify(this.state.diff[4])}</h5>
+            <h3>{JSON.stringify(this.state.sym[5])}</h3>
+            <p>Current Price</p>
+            <h5>{JSON.stringify(this.state.diff[5])}</h5>
+            <h3>{JSON.stringify(this.state.sym[6])}</h3>
+            <p>Current Price</p>
+            <h5>{JSON.stringify(this.state.diff[6])}</h5>
+            <h3>{JSON.stringify(this.state.sym[7])}</h3>
+            <p>Current Price</p>
+            <h5>{JSON.stringify(this.state.diff[7])}</h5>
+            <h3>{JSON.stringify(this.state.sym[8])}</h3>
+            <p>Current Price</p>
+            <h5>{JSON.stringify(this.state.diff[8])}</h5>
+            <h3>{JSON.stringify(this.state.sym[9])}</h3>
+            <p>Current Price</p>
+            <h5>{JSON.stringify(this.state.diff[9])}</h5>
         </div>
     )
   }
