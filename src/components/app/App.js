2292872378ce154f0275ebc0d72fb59dea8c45b4
SeanW17 <sean.wilson@aquaq.co.uk>
2021-08-06 14:40:06 +0100

    Updating App.js to save changes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,14 +1,5 @@
 import './App.css';
 import React from 'react';
-import LastValueCache from './LastValueCache';
-import MaxPriceSym from './MaxPriceSym';
-import MinPriceSym from './MinPriceSym';
-//import { BarChart, Bar, CartesianGrid, XAxis, YAxis } from 'recharts';
-//import chart1 from './chart1.js'
-//import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-
-
 
 export default class App extends React.Component {
 
@@ -19,43 +10,43 @@ export default class App extends React.Component {
         sym : "",
         amount: 0
     }
-  
-  
-  
 }
 
-async  componentDidMount() {
-
-  const url = "https://homer.aquaq.co.uk:8040/executeFunction";
-  const response = await 
-  fetch (url,{
-        "body": JSON.stringify({
-          "arguments": {
-         "db":"rdb",
-         "query":"select from (select from select sum size by sym from trade) where size = max size"},
-          "function_name": ".aqrest.execute"
-        }),
-          method:"post",
-          "headers": {
-            'Accept': 'application/json',
-            "Content-Type":"application/json",
-            "accept": "*/*",
-            "Authorization":"Basic dXNlcjpwYXNz"
-    }}
-    ) 
-    const data = await response.json();
-    console.log(data.result[0].sym)
-    this.setState({sym: data.result[0].sym})
-    this.setState({size: data.result[0].size})
-
+async componentDidMount() {
+    const url = "https://homer.aquaq.co.uk:8040/executeFunction";
+    try {
+      setInterval(async () => {
+        const response = await 
+        fetch (url,{
+              "body": JSON.stringify({
+                "arguments": {
+              "db":"rdb",
+              "query":"select from (select from select sum size by sym from trade) where size = max size"},
+                "function_name": ".aqrest.execute"
+              }),
+              method:"post",
+              "headers": {
+                'Accept': 'application/json',
+                "Content-Type":"application/json",
+                "accept": "*/*",
+                "Authorization":"Basic dXNlcjpwYXNz"
+        }}
+        ) 
+        const data = await response.json();
+        console.log(data.result[0].sym)
+        this.setState({sym: data.result[0].sym})
+        this.setState({size: data.result[0].size})
+      },1000);
+    } catch(e) {
+      console.log(e);
+    }
 }
-
 
 
 render() {
     return (
-        <div className='App'>
-          <table border="10" cellpadding="10">
+        <div>
+          <table>
             <tr>
               <th>Most Traded Sym</th>
               <th>Volume Traded</th>
@@ -65,9 +56,7 @@ render() {
               <td>{JSON.stringify(this.state.size)}</td>
             </tr>
           </table>
-          <div><MaxPriceSym/></div>
-          <div><MinPriceSym/></div>
-          <LastValueCache />
+          <div></div>
         </div>
     )
   }
