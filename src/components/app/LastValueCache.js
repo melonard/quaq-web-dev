d72bbc075f1073ac490e328dd73b50f8365f0ab3
KateJamison <kate.jamison@aquaq.co.uk>
2021-08-09 14:29:11 +0100

    Update.2

--- src/components/app/LastValueCache.js
+++ src/components/app/LastValueCache.js
@@ -15,53 +15,41 @@ constructor () {
 async  componentDidMount() {
 
     const url = "https://homer.aquaq.co.uk:8040/executeFunction";
-    try {
-        setInterval(async () => {
-            const response = await 
-            fetch (url,{
-                "body": JSON.stringify({
-                    "arguments": {
-                    "db":"rdb",
-                    "query": "select diff:last price - first price by sym from trade"},
-                    "function_name": ".aqrest.execute"
-                    }),
-                method:"post",
-                "headers": {
-                    'Accept': 'application/json',
-                    "Content-Type":"application/json",
-                    "accept": "*/*",
-                    "Authorization":"Basic dXNlcjpwYXNz"
-            }}
-            ) 
-            const data = await response.json();
-            
-            this.setState({all_data: data.result})
-            var diffArr=[]
-            var symArr=[]
-            for (let i = 0;i<10;i++){
-                diffArr.push(data.result[i].diff)
-                symArr.push(data.result[i].sym)
-            }
-            this.setState({sym: symArr})
-            this.setState({diff: diffArr})
-        },1000);
-        } catch(e) {
-        console.log(e);
-        }
-    //console.log(this.state.all_data)
+    const response = await 
+    fetch (url,{
+          "body": JSON.stringify({
+            "arguments": {
+            "db":"rdb",
+            "query": "select diff:first price - last price by sym from trade"},
+            "function_name": ".aqrest.execute"
+            }),
+          method:"post",
+          "headers": {
+            'Accept': 'application/json',
+            "Content-Type":"application/json",
+            "accept": "*/*",
+            "Authorization":"Basic dXNlcjpwYXNz"
+    }}
+    ) 
+    const data = await response.json();
+    console.log(data)
+    this.setState({all_data: data.result})
+    for (let i = 0;i<10;i++){
+        this.setState({sym:[...this.state.sym ,data.result[i].sym]})
+        this.setState({diff: [...this.state.diff,data.result[i].diff]})
+    }
 }
 
-// for (let i = 0;i<10;i++){
-// if (this.state.diff[i] >= 0) {
-//     this.state.diff[i].color = "green";
+// if (this.state.diff >= 0) {
+//     this.state.diff.color = "green";
 // } else {
-//     this.state.diff[i].color = "red";
-// }
+//     this.state.diff.color = "red";
 // }
 
 render() {
     return (
         <div className="LVC">
+            <div>{JSON.stringify(this.state.all_data)}</div>
             <h3>{JSON.stringify(this.state.sym[0])}</h3>
             <p>Current Price</p>
             <h5>{JSON.stringify(this.state.diff[0])}</h5>
