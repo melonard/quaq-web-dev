06897c6e209fbc45f0c9f6c529bfbccdecd875e5
KateJamison <kate.jamison@aquaq.co.uk>
2021-08-17 13:38:03 +0100

    % Added for LVC

--- src/components/app/LastValueCache.js
+++ src/components/app/LastValueCache.js
@@ -10,7 +10,9 @@ constructor () {
         diff:[],
         curr:[],
         openDiff:[],
-        result:[]
+        result:[],
+        LVCpercent:[],
+        percent:[]
     }
   
 }
@@ -42,18 +44,27 @@ async  componentDidMount() {
             var symArr=[]
             var currArr=[]
             var diffArr=[]
-            var openDiffArr=[]
+            var openDiffArr=[] 
+            var LVCpercentArr=[]
+            var percentArr=[]
+
             for (let i = 0;i<10;i++){
                 symArr.push(data.result[i].sym)
                 currArr.push(data.result[i].curr)
                 diffArr.push(data.result[i].diff)
-                openDiffArr.push(data.result[i].openDiff)
+                openDiffArr.push(data.result[i].openDiff) 
+                percentArr.push(100* data.result[i].openDiff /data.result[i].curr) 
+                LVCpercentArr.push(100* data.result[i].diff /data.result[i].curr)              
             }
+      
             this.setState({sym: symArr})
             this.setState({curr: currArr.map(ele => parseFloat(ele.toFixed(2)))})
             this.setState({diff: diffArr.map(ele => parseFloat(ele.toFixed(2)))})
             this.setState({openDiff: openDiffArr.map(ele => parseFloat(ele.toFixed(2)))})
-            this.setState({date: new Date().toLocaleString()})
+            this.setState({date: new Date().toLocaleString()}) 
+            this.setState({LVCpercent: LVCpercentArr.map(ele => parseFloat(ele.toFixed(2)))})
+            this.setState({percent: percentArr.map(ele => parseFloat(ele.toFixed(2)))})
+
         },1000);
         } catch(e) {
         console.log(e);
@@ -64,23 +75,24 @@ async  componentDidMount() {
 render() {
     return (
         <div>   
-                <h3>Stock Price Fluxuation</h3>
+                <h3>Stock Price Fluxuation
+                </h3>
                 <div className="date">
                 <p> Last Updated: {this.state.date}</p>
                 </div>
                  <div className='App'>
                       <table border="10" cellPadding="10">
-                        <col width="100px" />
-                        <col width="100px" />
-                        <col width="100px" />
-                        <col width="100px" />
-                        <col width="100px" />
-                        <col width="100px" />
-                        <col width="100px" />
-                        <col width="100px" />
-                        <col width="100px" />
-                        <col width="100px" />
-                        <col width="100px" />
+                        <col width="120px" />
+                        <col width="110px" />
+                        <col width="110px" />
+                        <col width="110px" />
+                        <col width="110px" />
+                        <col width="110px" />
+                        <col width="110px" />
+                        <col width="110px" />
+                        <col width="110px" />
+                        <col width="110px" />
+                        <col width="110px" />
                         <tr>
                         <h4>Stock</h4>
                             {this.state.sym.map((entry,index)=>{
@@ -99,10 +111,26 @@ render() {
                                 return(<td>{entry > 0 ? <font color="green">+{entry} <AiOutlineArrowUp/></font> : <font color="red">{entry} <AiOutlineArrowDown/></font>}</td>)
                             })}
                         </tr>
+
+                        <tr>
+                        <h4>% Difference LVC</h4>
+
+                            {this.state.LVCpercent.map((entry,index)=>{
+                                return(<td>{entry > 0 ? <font color="green">({entry}%) </font> : <font color="red">({entry}%) </font>}</td>)
+                            })}
+                        </tr>
+
                         <tr>
                         <h4>Difference from Open</h4>
                             {this.state.openDiff.map((entry,index)=>{
                                 return(<td>{entry > 0 ? <font color="green">+{entry} <AiOutlineArrowUp/></font> : <font color="red">{entry} <AiOutlineArrowDown/></font>}</td>)
+                            })}
+                            </tr>
+                        <tr>
+                        <h4>% Difference from Open</h4>
+
+                            {this.state.percent.map((entry,index)=>{
+                                return(<td>{entry > 0 ? <font color="green">({entry}%) </font> : <font color="red">({entry}%) </font>}</td>)
                             })}
                         </tr>
                       </table>  
@@ -111,3 +139,4 @@ render() {
     )
   }
 }
+//<span>&#37;</span>
